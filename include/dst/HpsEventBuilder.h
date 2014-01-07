0e548c254798986890e3e9dd951167cc8da96ce0
Omar Moreno <email@omarmoreno.net>
2014-01-06 23:06:01 -0800

    Implementation of EventBuilder used to convert data from the Full HPS run to HpsEvents

---
+++ include/dst/HpsEventBuilder.h
@@ -0,0 +1,33 @@
+/**
+ *	@section purpose: 
+ *	@author: Omar Moreno <omoreno1@ucsc.edu>
+ *			 Santa Cruz Institute for Particle Physics
+ *			 University of California, Santa Cruz
+ *	@date: December 31, 2013
+ *	@version: 1.0
+ *
+ */
+
+#ifndef __HPS_EVENT_BUILDER_H__
+#define __HPS_EVENT_BUILDER_H__
+
+//--- DST ---//
+//-----------//
+#include <EventBuilder.h>
+#include <SvtDataWriter.h>
+
+class HpsEventBuilder : public EventBuilder {
+
+	public: 
+		HpsEventBuilder(); 
+		~HpsEventBuilder(); 
+
+		void makeHpsEvent(EVENT::LCEvent*, HpsEvent*); 
+	
+	private:
+
+		SvtDataWriter* svt_writer; 
+
+};
+
+#endif // __HPS_EVENT_BUILDER_H__
