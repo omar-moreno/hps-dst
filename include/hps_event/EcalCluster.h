08591ff36ba4c56df2833349fe0d6f196313f0d5
Omar Moreno <omoreno1@ucsc.edu>
2014-01-08 17:35:21 -0800

    Change some variable names so they are clearer

--- include/hps_event/EcalCluster.h
+++ include/hps_event/EcalCluster.h
@@ -4,7 +4,7 @@
  * @section institution
  * 				Santa Cruz Institute for Particle Physics
  * 				University of California, Santa Cruz
- * @version:    v 0.1
+ * @version:    v 1.0
  * @date:       February 19, 2013
  */
 
@@ -12,18 +12,21 @@
 #define _ECAL_CLUSTER_H_
 
 //--- C++ ---//
+//-----------//
 #include <iostream>
 
 //-- ROOT ---//
+//-----------//
 #include <TObject.h>
 #include <TClonesArray.h>
 
 class EcalCluster : public TObject {
 
-    public: 
+    public:
+
         EcalCluster();
         EcalCluster(const EcalCluster &ecalClusterObj);
-        virtual ~EcalCluster();
+        ~EcalCluster();
         EcalCluster &operator=(const EcalCluster &ecalClusterObj);
 
         void Clear(Option_t *option="");
@@ -32,8 +35,8 @@ class EcalCluster : public TObject {
         void setClusterEnergy(double energy)    { this->energy = energy; };
         void setNumberOfEcalHits(int n_hits)    { this->n_ecal_hits = n_hits; };
         void setClusterHitTime(double hit_time) { this->hit_time = hit_time; };
-        void setHighestEnergyHit(double high_energy) { this->high_energy = high_energy; };
-        void setHighestEnergyHitPosition(double*);
+        void setSeedEnergy(double seed_energy)  { this->seed_energy = seed_energy; };
+        void setSeedPosition(double*);
         void setM2(double m2) { this->m2 = m2; };
         void setM3(double m3) { this->m3 = m3; };
 
@@ -42,9 +45,9 @@ class EcalCluster : public TObject {
         double getYPosition()		  const { return y; };
         double getZPosition()		  const { return z; };
         double getClusterEnergy()	  const { return energy; };
-        double getHighestEnergyHit()  const { return high_energy; };
-        double getHighestEnergyHitX() const { return x_high_energy; };
-        double getHighestEnergyHitY() const { return y_high_energy; };
+        double getSeedEnergy()  const { return seed_energy; };
+        double getSeedX() const { return seed_x; };
+        double getSeedY() const { return seed_y; };
         double getM2() const { return m2; };
         double getM3() const { return m3; };
 
@@ -58,10 +61,10 @@ class EcalCluster : public TObject {
         double z;
         double energy; 
         double hit_time;
-        double high_energy;
-        double x_high_energy;
-        double y_high_energy;
-        double z_high_energy;
+        double seed_energy;
+        double seed_x;
+        double seed_y;
+		double seed_z; 
         double m2;
         double m3;
 
