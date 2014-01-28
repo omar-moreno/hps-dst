60a9693e017baa501fcee5c50682818b9dde98b4
Omar Moreno <email@omarmoreno.net>
2014-01-28 15:46:16 -0800

    Implementation of CalorimeterHit for the HPS Ecal; Update LinkDef with new class

---
+++ include/hps_event/EcalHit.h
@@ -0,0 +1,47 @@
+/**
+ *	@author:	Omar Moreno <omoreno1@ucsc.edu>
+ *	@section institution
+ *				Santa Cruz Institute for Particle Physics
+ *				University of California, Santa Cruz
+ *	@version:	v 1.0
+ *	@date:		January 28, 2014
+ *
+ */
+
+#ifndef _ECAL_HIT_H_
+#define _ECAL_HIT_H_
+
+//--- ROOT ---//
+//------------//
+#include <TObject.h>
+
+//--- HPS Event ---//
+//-----------------//
+#include <CalorimterHit.h>
+
+class EcalHit : public CalorimeterHit, public TObject { 
+
+	public: 
+		EcalHit(); 
+		~EcalHit();
+		
+		void Clear(Option_t* option=""); 
+		
+		void setPosition(const std::vector<double>); 
+		void setEnergy(const double energy){ this->energy = energy; }; 
+
+		std::vector<double> getPosition(); 
+		double getEnergy() const { return energy; };  
+
+		ClassDef(EcalHit, 1); 
+
+	private: 
+
+		double energy;
+
+		double hit_x; 
+		double hit_y; 
+		double hit_z; 
+};
+
+#endif
