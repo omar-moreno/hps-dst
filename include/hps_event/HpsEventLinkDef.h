bf95f30c16346df1986e6a61097365328b99534c
Omar Moreno <omoreno1@ucsc.edu>
2014-01-31 13:23:25 -0800

    Add the Cluster and CalorimeterHit interfaces to the ROOT dictionary

--- include/hps_event/HpsEventLinkDef.h
+++ include/hps_event/HpsEventLinkDef.h
@@ -3,6 +3,7 @@
 #pragma link off all globals; 
 #pragma link off all classes; 
 #pragma link off all functions; 
+#pragma link C++ nestedclasses;
 
 #pragma link C++ class HpsEvent+;
 #pragma link C++ class EcalCluster+;
@@ -10,6 +11,8 @@
 #pragma link C++ class SvtTrack+;
 #pragma link C++ class SvtHit+;
 #pragma link C++ class HpsReconstructedParticle+;
+#pragma link C++ class CalorimeterHit+;
+#pragma link C++ class Cluster+;
 #pragma link C++ class EcalHit+;
 
 #endif
