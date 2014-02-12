5a318e9ef80e54c94afa22f5bcddb720f64d4cdf
Omar Moreno <omoreno1@ucsc.edu>
2014-02-12 13:25:13 -0800

    Add HpsMCParticle and MCParticle to the ROOT dictionary

--- include/hps_event/HpsEventLinkDef.h
+++ include/hps_event/HpsEventLinkDef.h
@@ -7,12 +7,14 @@
 
 #pragma link C++ class HpsEvent+;
 #pragma link C++ class EcalCluster+;
+#pragma link C++ class Cluster+;
 #pragma link C++ class MuonCluster+;
 #pragma link C++ class SvtTrack+;
 #pragma link C++ class SvtHit+;
 #pragma link C++ class HpsReconstructedParticle+;
 #pragma link C++ class CalorimeterHit+;
-#pragma link C++ class Cluster+;
 #pragma link C++ class EcalHit+;
+#pragma link C++ class MCParticle+;
+#pragma link C++ class HpsMCParticle;
 
 #endif
