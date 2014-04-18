b426850b85c460f5ef4235dbe49242f09e3580ac
Omar Moreno <email@omarmoreno.net>
2014-04-18 16:47:40 -0700

    Started adding HpsParticles to HpsEvent.  Still a work in progress ...

--- include/dst/HpsParticleDataWriter.h
+++ include/dst/HpsParticleDataWriter.h
@@ -10,6 +10,15 @@
 #ifndef __HPS_PARTICLE_WRITER_H__
 #define __HPS_PARTICLE_WRITER_H__
 
+//--- C++ ---//
+//-----------//
+#include <string>
+
+//--- LCIO ---//
+//------------//
+#include <IMPL/LCCollectionVec.h>
+#include <IMPL/ReconstructedParticleImpl.h>
+
 //--- DST ---//
 //-----------//
 #include <DataWriter.h>
@@ -27,6 +36,18 @@ class HpsParticleDataWriter : public DataWriter {
 
         //
         void writeData(EVENT::LCEvent*, HpsEvent*);
+
+	private:
+
+		std::string fs_particles_collection_name;
+		std::string uc_vtx_particles_collection_name; 
+		std::string bsc_vtx_particles_collection_name; 
+		std::string tc_vtx_particles_collection_name; 
+
+		IMPL::LCCollectionVec* particles;
+		IMPL::ReconstructedParticleImpl* particle; 
+
+		HpsParticle* hps_particle; 
 
 }; // HpsParticleWriter
 
--- src/dst/HpsParticleDataWriter.cxx
+++ src/dst/HpsParticleDataWriter.cxx
@@ -9,18 +9,99 @@
 
 #include <HpsParticleDataWriter.h>
 
-HpsParticleDataWriter::HpsParticleDataWriter(){}
+HpsParticleDataWriter::HpsParticleDataWriter() 
+	: fs_particles_collection_name("FinalStateParticles"),
+	  uc_vtx_particles_collection_name("AprimeUnconstrained"),
+	  bsc_vtx_particles_collection_name("AprimeBeamspotConstrained"),
+	  tc_vtx_particles_collection_name("AprimeTargetConstrained"),
+	  particles(NULL), particle(NULL), hps_particle(NULL)
+{
 
-HpsParticleDataWriter::~HpsParticleDataWriter(){}
+}
+
+HpsParticleDataWriter::~HpsParticleDataWriter()
+{
+	delete particles;
+	delete particle;
+	delete hps_particle;
+}
 
 void HpsParticleDataWriter::writeData(EVENT::LCEvent* event, HpsEvent* hps_event)
 {
     // Get the collection of particles from the event. If the event doesn't 
     // have particles, return;
     try { 
-    } catch { 
-       
+    
+		particles = (IMPL::LCCollectionVec*) event->getCollection(fs_particles_collection_name);
+		
+		for(int particle_n = 0; particle_n < particles->getNumberOfElements(); ++particle_n){
+		
+			particle = (IMPL::ReconstructedParticleImpl*) particles->getElementAt(particle_n); 
 
+			hps_particle = hps_event->addFSParticle();
 
+			if(particle->getClusters().size() > 0){
+				for(int cluster_n = 0; cluster_n < hps_event->getNumberOfEcalClusters(); ++cluster_n){
+					if(particle->getClusters()[0]->getEnergy() == hps_event->getEcalCluster(cluster_n)->getClusterEnergy()){
+						hps_particle->setEcalCluster(hps_event->getEcalCluster(cluster_n)); 
+					}
+				}
+			}
+
+			if(particle->getTracks().size() > 0){
+				
+				for(int track_n = 0; track_n < hps_event->getNumberOfTracks(); ++track_n){
+					if(particle->getTracks()[0]->getChi2() == hps_event->getTrack(track_n)->getChi2()){
+						hps_particle->setTrack(hps_event->geTrack(track_n));
+					}
+				}
+			}	
+		}
+		
+	} catch(DataNotAvailableException std::exception) { 
+			
+	}
+	
+	try { 
+	
+		particles = (IMPL::LCCollectionVec*) event->getCollection(uc_vtx_particles_collection_name);
+		
+		for(int particle_n = 0; particle_n < particles->getNumberOfElements(); ++particle_n){
+		
+			particle = (IMPL::ReconstructedParticleImpl*) particles->getElementAt(particle_n); 
+
+			hps_particle = hps_event->addVtxParticle(HpsEvent::UC_VTX_PARTICLES_INDEX);	
+		}
+	
+	} catch(DataNotAvailableException std::exception) { 
+			
+	}
+
+	try {	
+		particles = (IMPL::LCCollectionVec*) event->getCollection(bsc_vtx_particles_collection_name);
+		
+		for(int particle_n = 0; particle_n < particles->getNumberOfElements(); ++particle_n){
+		
+			particle = (IMPL::ReconstructedParticleImpl*) particles->getElementAt(particle_n); 
+
+			hps_particle = hps_event->addVtxParticle(HpsEvent::BSC_VTX_PARTICLES_INDEX);	
+		}
+		
+	} catch(DataNotAvailableException std::exception) { 
+	
+	}
+
+	try {
+		particles = (IMPL::LCCollectionVec*) event->getCollection(tc_vtx_particles_collection_name);
+		
+		for(int particle_n = 0; particle_n < particles->getNumberOfElements(); ++particle_n){
+		
+			particle = (IMPL::ReconstructedParticleImpl*) particles->getElementAt(particle_n); 
+
+			hps_particle = hps_event->addVtxParticle(HpsEvent::TC_VTX_PARTICLES_INDEX);	
+		}
+
+	} catch(DataNotAvailableException std::exception) { 
+	
     }
 }
