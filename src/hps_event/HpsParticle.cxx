2183e60746808ea168350855a20e1c4b342985fb
Omar Moreno <email@omarmoreno.net>
2014-06-03 00:13:45 -0700

    Add momentum and charge variables to HpsParticle along with setters and getters.

--- src/hps_event/HpsParticle.cxx
+++ src/hps_event/HpsParticle.cxx
@@ -13,14 +13,16 @@ ClassImp(HpsParticle)
 
 HpsParticle::HpsParticle()
 	: TObject(), svt_tracks(new TRefArray()), ecal_clusters(new TRefArray()),
-	  n_daughters(0), vtx_x(0), vtx_y(0), vtx_z(0)
+	  n_daughters(0), charge(0), 
+      px(0), py(0), pz(0), vtx_x(0), vtx_y(0), vtx_z(0)
 
 {}
 
 HpsParticle::HpsParticle(const HpsParticle &particleObj)
 	: TObject(), svt_tracks(new TRefArray()), ecal_clusters(new TRefArray()),
-	  n_daughters(particleObj.n_daughters), vtx_x(particleObj.vtx_x),
-	  vtx_y(particleObj.vtx_y), vtx_z(particleObj.vtx_z)
+	  n_daughters(particleObj.n_daughters), charge(particleObj.charge),
+      px(particleObj.px), py(particleObj.py), pz(particleObj.pz), 
+      vtx_x(particleObj.vtx_x), vtx_y(particleObj.vtx_y), vtx_z(particleObj.vtx_z)
 {
 	*svt_tracks = *particleObj.svt_tracks;
 	*ecal_clusters = *particleObj.ecal_clusters;	
@@ -42,7 +44,11 @@ HpsParticle &HpsParticle::operator=(const HpsParticle &particleObj)
 	Clear(); 
 
 	this->n_daughters = particleObj.n_daughters;
+    this->charge = particleObj.charge;
 
+    this->px = particleObj.px; 
+    this->py = particleObj.py; 
+    this->pz = particleObj.pz; 
 	this->vtx_x = particleObj.vtx_x;
 	this->vtx_y = particleObj.vtx_y;
 	this->vtx_z = particleObj.vtx_z;
@@ -74,7 +80,14 @@ void HpsParticle::addCluster(EcalCluster* ecal_cluster)
 	ecal_clusters->Add(ecal_cluster);
 }
 
-void HpsParticle::setVertexPosition(const double *vtx_pos)
+void HpsParticle::setMomentum(const double* momentum)
+{
+    px = momentum[0];
+    py = momentum[1];
+    pz = momentum[2];
+}
+
+void HpsParticle::setVertexPosition(const double* vtx_pos)
 {
 	vtx_x = vtx_pos[0];
 	vtx_y = vtx_pos[1];
@@ -89,6 +102,15 @@ TRefArray* HpsParticle::getTracks() const
 TRefArray* HpsParticle::getClusters() const
 {
 	return ecal_clusters;
+}
+
+std::vector<double> HpsParticle::getMomentum() const
+{
+    std::vector<double> momentum(3,0);
+    momentum[0] = px;
+    momentum[1] = py;
+    momentum[2] = pz;
+    return momentum;
 }
 
 std::vector<double> HpsParticle::getVertexPosition() const
