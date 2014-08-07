0bc2ed31dcd9e501bb385fc32173b1921ca13205
Omar Moreno <email@omarmoreno.net>
2014-08-06 18:42:40 -0700

    See previous comment.  Work in progress ...

--- src/hps_event/GblTrackData.cxx
+++ src/hps_event/GblTrackData.cxx
@@ -1,56 +1,84 @@
 /**
  * @section purpose: Stores track information needed by GBL
  * @author: 	Per Hansson Adrian <phansson@slac.stanford.edu>
- * @version:    v1.0
  * @date:       February 3, 2014
  */
 
 #include <GblTrackData.h>
-#include <GblStripData.h>
-
-//-- c++ --//
-#include <sstream>
-
-//-- ROOT --//
-//#include <TIter.h>
-
 
 ClassImp(GblTrackData)
 
-GblTrackData::GblTrackData()
-: 	TObject(), m_gbl_strip_hits(new TRefArray()), n_gbl_strip_hits(0), m_prjPerToCl(3,3)
+GblTrackData::GblTrackData() 
+	: TObject(), m_gbl_strip_hits(new TRefArray()), svt_track(NULL),
+	  n_gbl_strip_hits(0), m_prjPerToCl(3,3)
 {}
-
-void GblTrackData::Clear(Option_t* /* option */) {
-  TObject::Clear();
-  m_gbl_strip_hits->Delete();
-  n_gbl_strip_hits = 0;
-}
 
 GblTrackData::~GblTrackData()
 {
-  delete m_gbl_strip_hits;
+	delete m_gbl_strip_hits;
 }
 
-void GblTrackData::addStrip(GblStripData* strip) {
-  ++n_gbl_strip_hits;
-  m_gbl_strip_hits->Add(strip);
+void GblTrackData::Clear(Option_t* /* option */)
+{
+	TObject::Clear();
+	m_gbl_strip_hits->Delete();
+	n_gbl_strip_hits = 0;
+}
+
+void GblTrackData::addStrip(GblStripData* strip)
+{
+	++n_gbl_strip_hits;
+	m_gbl_strip_hits->Add(strip);
+}
+
+void GblTrackData::setPrjPerToCl(const unsigned int row, 
+								 const unsigned int col,
+								 const double val)
+{
+	m_prjPerToCl(row, col) = val;
+}
+
+double GblTrackData::getKappa() const
+{
+	// Note: Omega and Kappa both refer to the track
+	//		 curvature.
+	return ((SvtTrack*) svt_track.GetObject())->getOmega(); 
+}
+
+double GblTrackData::getTheta() const
+{
+	double tan_lambda = ((SvtTrack*) svt_track.GetObject())->getTanLambda();
+	return TMath::PiOver2() - atan(tan_lambda);
+}
+
+double GblTrackData::getPhi() const
+{
+	return ((SvtTrack*) svt_track.GetObject())->getPhi();
+}
+
+double GblTrackData::getD0() const
+{
+	return ((SvtTrack*) svt_track.GetObject())->getD0(); 
+}
+
+double GblTrackData::getZ0() const 
+{
+	return ((SvtTrack*) svt_track.GetObject())->getZ0();
 }
 
 std::string GblTrackData::toString() const {
-    std::ostringstream oss;
-    oss << "GblTrackData:\n";
-    oss << "\t" << "kappa " << getKappa() << "\n";
-    oss << "\t" << "theta " << getTheta() << "\n";
-    oss << "\t" << "phi   " << getPhi() << "\n";
-    oss << "\t" << "d0    " << getD0() << "\n";
-    oss << "\t" << "z0    " << getZ0() << "\n";
-    oss << "\t" << n_gbl_strip_hits << " GBL strips:\n";
-    TIter iter(m_gbl_strip_hits->MakeIterator());
-    const GblStripData* strip = NULL;
-    //TObject* obj = NULL;
-    while( (strip = (GblStripData*)iter()) != NULL) {
-      oss << strip->toString() << "\n";
-    }
-    return oss.str();
-  }
+	std::ostringstream oss;
+	oss << "GblTrackData:\n";
+	oss << "\t" << "kappa " << getKappa() << "\n";
+	oss << "\t" << "theta " << getTheta() << "\n";
+	oss << "\t" << "phi   " << getPhi() << "\n";
+	oss << "\t" << "d0    " << getD0() << "\n";
+	oss << "\t" << "z0    " << getZ0() << "\n";
+	oss << "\t" << n_gbl_strip_hits << " GBL strips:\n";
+	TIter iter(m_gbl_strip_hits->MakeIterator());
+	const GblStripData* strip = NULL;
+	while( (strip = (GblStripData*)iter()) != NULL) {
+		oss << strip->toString() << "\n";
+	}
+	return oss.str();
+}
