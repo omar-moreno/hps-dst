b2b4d0e5bff93e8492fd0970256ed65ce6eb1907
Omar Moreno <email@omarmoreno.net>
2014-12-31 00:21:48 -0800

    Allow the HpsEventBuilder to process recon files containing ECal data only.

--- src/dst/HpsEventBuilder.cxx
+++ src/dst/HpsEventBuilder.cxx
@@ -1,26 +1,25 @@
 /**
- *	@section purpose: 
- *	@author: Omar Moreno <omoreno1@ucsc.edu>
- *			 Santa Cruz Institute for Particle Physics
- *			 University of California, Santa Cruz
- *	@date: January 6, 2014
+ *	@file   HpsEventBuilder.cxx
+ *	@brief
+ *	@author Omar Moreno <omoreno1@ucsc.edu>
+ *		    Santa Cruz Institute for Particle Physics
+ *			University of California, Santa Cruz
+ *	@date   January 6, 2014
  *
  */
 
 #include <HpsEventBuilder.h>
 
-HpsEventBuilder::HpsEventBuilder() : m_run_gbl(false)
-{
-	svt_writer = new SvtDataWriter(); 
-	ecal_writer = new EcalDataWriter();
-	mc_particle_writer = new MCParticleDataWriter();
-	particle_writer = new HpsParticleDataWriter(); 
-	gbl_data_writer  = new GblDataWriter();
-	gbl_track_writer  = new GblTrackWriter();
-}
+HpsEventBuilder::HpsEventBuilder() 
+    : svt_writer(new SvtDataWriter()), ecal_writer(new EcalDataWriter()),
+      mc_particle_writer(new MCParticleDataWriter()), 
+      particle_writer(new HpsParticleDataWriter()),
+      gbl_data_writer(new GblDataWriter()),
+      gbl_track_writer(new GblTrackWriter()),
+      run_gbl(false), ecal_only(false)
+{}
 
-HpsEventBuilder::~HpsEventBuilder()
-{
+HpsEventBuilder::~HpsEventBuilder() {
 	delete svt_writer;
 	delete ecal_writer; 	
 	delete mc_particle_writer;
@@ -29,9 +28,10 @@ HpsEventBuilder::~HpsEventBuilder()
 	delete gbl_track_writer;
 }
 
-void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event)
-{
-	hps_event->Clear(); 
+void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event) {
+	
+    // Clear the HpsEvent from all previous information
+    hps_event->Clear(); 
 
 	// Set the event number
 	hps_event->setEventNumber(event->getEventNumber());
@@ -39,41 +39,33 @@ void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event)
 	// Set the run number
 	hps_event->setRunNumber(event->getRunNumber());
 
-	// Write SVT data to HpsEvent
-	svt_writer->writeData(event, hps_event);
-
-	// Write Ecal data to HpsEvent
+	// Write Ecal data to the HpsEvent
 	ecal_writer->writeData(event, hps_event); 
 
-	// Write MC particle data to HpsEvent
+    // If only Ecal data is going to be written to the DST, skip the rest
+    // of the writers
+    if (ecal_only) return;
+
+	// Write SVT data to the HpsEvent
+	svt_writer->writeData(event, hps_event);
+
+	// Write MC particle data to the HpsEvent
 	mc_particle_writer->writeData(event, hps_event);
 
-	// Write the HpsParticle data to HpsEvent
+	// Write the HpsParticle data to the HpsEvent
 	particle_writer->writeData(event, hps_event); 
 
-	// Write GBL data to HpsEvent
-	if( getGblFlag() == true ) {
+	// If GBL has been enabled, process the SVT tracks using GBL and write the
+    // data to the HpsEvent
+    if (!run_gbl) return;
 
-		// Write info used for GBL to HpsEvent
-		gbl_data_writer->writeData(event, hps_event);
+	// Write info used for GBL to the HpsEvent
+	gbl_data_writer->writeData(event, hps_event);
 
-		// Write GBL track refit to HpsEvent    
-		gbl_track_writer->writeData(hps_event);
-
-	}
+	// Write GBL track refit to the HpsEvent    
+	gbl_track_writer->writeData(hps_event);
 }
 
-void HpsEventBuilder::setBField(const double b_field)
-{
+void HpsEventBuilder::setBField(const double b_field) {
 	gbl_track_writer->setBField(b_field);
-}
-
-void HpsEventBuilder::setGblFlag(const bool flag)
-{
-	m_run_gbl = flag;
-}
-
-bool HpsEventBuilder::getGblFlag() const
-{
-	return m_run_gbl;
 }
