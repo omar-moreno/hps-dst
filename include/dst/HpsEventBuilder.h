35d6d300476d86cab07dcd8b3beb1b55d0d4768d
Omar Moreno <omoreno1@ucsc.edu>
2014-12-31 00:21:48 -0800

    Allow the HpsEventBuilder to process recon files containing ECal data only.

--- include/dst/HpsEventBuilder.h
+++ include/dst/HpsEventBuilder.h
@@ -1,15 +1,17 @@
 /**
- *	@section purpose: 
- *	@author: Omar Moreno <omoreno1@ucsc.edu>
- *			 Santa Cruz Institute for Particle Physics
- *			 University of California, Santa Cruz
- *	@date: December 31, 2013
+ *	@file   HpsEventBuilder.h
+ *	@author Omar Moreno <omoreno1@ucsc.edu>
+ *		    Santa Cruz Institute for Particle Physics
+ *		    University of California, Santa Cruz
+ *	@date   December 31, 2013
  *
  */
 
 #ifndef __HPS_EVENT_BUILDER_H__
 #define __HPS_EVENT_BUILDER_H__
 
+
+//-----------//
 //--- DST ---//
 //-----------//
 #include <EventBuilder.h>
@@ -23,17 +25,48 @@
 class HpsEventBuilder : public EventBuilder {
 
 	public: 
+
+        /**
+         *  Default Constructor
+         */
 		HpsEventBuilder(); 
+
+        /**
+         *  Destructor
+         */
 		~HpsEventBuilder(); 
 
-		//
-		void makeHpsEvent(EVENT::LCEvent*, HpsEvent*); 
+		/**
+         *  Write collections present in the LCEvent to an HpsEvent.
+         *
+         *  @param event - The LCEvent to process
+         *  @param hps_event - The HpsEvent to write the LCEvent data to
+         */
+		void makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event); 
 
-		// 
+		/**
+         *  Set the strenght of the magnetic field during the run.  This is 
+         *  currently used by GBL only and will be removed when the B field
+         *  information can be retrieved from the recon file.
+         *
+         *  @param b_field - The strength of the magnetic field in tesla
+         */
 		void setBField(const double);
-		void setGblFlag(const bool);
 		
-		bool getGblFlag() const;
+        /**
+         *  Enable/disble the processing of SVT tracks with GBL.
+         *
+         *  @param run_gbl - true to enable GBL, false otherwise
+         */
+        void runGbl(const bool run_gbl) { this->run_gbl = run_gbl; };
+	
+        /**
+         *  Enable/disable the creation of an HpsEvent using recon files 
+         *  containing ECal data only.
+         *
+         *  @param ecal_only - true to write ECal data only, false otherwise
+         */    
+        void writeEcalOnly(const bool ecal_only) { this->ecal_only = ecal_only; }; 
 
 	private:
 
@@ -44,7 +77,9 @@ class HpsEventBuilder : public EventBuilder {
 		GblDataWriter* gbl_data_writer;
 		GblTrackWriter* gbl_track_writer;
 
-		bool m_run_gbl;
-};
+		bool run_gbl;
+        bool ecal_only;
+
+}; // HpsEventBuilder
 
 #endif // __HPS_EVENT_BUILDER_H__
