5c5d51fd3f93268b88bf59671484456cca572f3b
Omar Moreno <email@omarmoreno.net>
2015-05-12 23:30:15 -0700

    Add a constructor that takes an LCIO track object as an argument.  Work in progress.

--- include/hps_event/SvtTrack.h
+++ include/hps_event/SvtTrack.h
@@ -1,58 +1,160 @@
 /**
- *
- * @author: 	Omar Moreno <omoreno1@ucsc.edu>
- * @section institution
- * 				Santa Cruz Institute for Particle Physics
- * 				University of California, Santa Cruz
- * @date:       February 19, 2013
- *
+ * @file: SvtTrack.h
+ * @author: Omar Moreno <omoreno1@ucsc.edu>
+ * @section Institution \n
+ *          Santa Cruz Institute for Particle Physics
+ *          University of California, Santa Cruz
+ * @date:   February 19, 2013
  */
 
 #ifndef __SVT_TRACK_H__
 #define __SVT_TRACK_H__
 
-//--- C++ ---//
-//-----------//
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
 #include <iostream>
 
+//------------//
 //--- ROOT ---//
 //------------//
 #include <TObject.h>
 #include <TClonesArray.h>
 #include <TRefArray.h>
 
+//------------//
+//--- LCIO ---//
+//------------//
+#include <EVENT/Track.h>
+
+//-----------------//
 //--- HPS Event ---//
 //-----------------//
 #include <SvtHit.h>
 
 class SvtTrack : public TObject {
 
+    // TODO: Add more documentation
+
     public:
 
-		SvtTrack();
+        /**
+         * Default Constructor
+         */
+        SvtTrack();
+
+        /**
+         * Constructor
+         *
+         * @param track : An LCIO Track object
+         */
+        SvtTrack(const EVENT::Track* track);
+
+        /**
+         * Copy constructor
+         *
+         * @param svtTrackObj : An SvtTrack object
+         */
         SvtTrack(const SvtTrack &svtTrackObj);
+
+        /**
+         * Destructor
+         */
         ~SvtTrack();
+
+        /**
+         *
+         */
         SvtTrack &operator=(const SvtTrack &svtTrackObj);
-        
+       
+        /**
+         *
+         */ 
         void Clear(Option_t *option="");
 
-        void addHit(SvtHit*); 
+        /**
+         * Add a reference to an SvtHit.
+         *
+         * @param hit : An SvtHit
+         */
+        void addHit(SvtHit* hit); 
 
-        void setTrackParameters(double, double, double, double, double);
+        /**
+         * Set the track parameters.
+         *
+         * @param d0 : Distance of closest approach
+         * @param phi : 
+         * @param omega : 
+         * @param tan_lambda : 
+         * @param z0 : 
+         */
+        void setTrackParameters(double d0, double phi, double omega, double tan_lambda, double z0);
+        
+        /**
+         * Set the chi^2 of the fit to the track.
+         *
+         * @param chi_squared : The chi^2 of the fit to the track.
+         */
         void setChi2(double chi_squared){ this->chi_squared = chi_squared; };
+        
+        /**
+         *
+         */
         void setL1Isolation(double l1_isolation) {this->l1_isolation = l1_isolation; };
+        
+        /**
+         *
+         */
         void setL2Isolation(double l2_isolation) {this->l2_isolation = l2_isolation; };
 
+        /**
+         *
+         */
         double getD0() const { return d0; };
+        
+        /**
+         *
+         */
         double getPhi() const { return phi; };
+        
+        /**
+         *
+         */
         double getOmega() const { return omega; };
+        
+        /**
+         *
+         */
         double getTanLambda() const { return tan_lambda; };
+        
+        /**
+         *
+         */
         double getZ0() const { return z0; };
+        
+        /**
+         *
+         */
         double getChi2() const {return chi_squared; };
+        
+        /**
+         *
+         */
         double getL1Isolation() const {return l1_isolation; };
+        
+        /**
+         *
+         */
         double getL2Isolation() const {return l2_isolation; };
+        
+        /**
+         *
+         */
         TRefArray* getSvtHits() const;
 
+        /**
+         *
+         */
         ClassDef(SvtTrack, 1);
 
     private:
