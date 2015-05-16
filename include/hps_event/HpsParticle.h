a2c2faa9d8c4d80d1fdaf657c752f2fad81a92ec
Omar Moreno <email@omarmoreno.net>
2015-05-15 22:25:12 -0700

    Forward declare SvtTrack and EcalCluster.  Some cleanup.

--- include/hps_event/HpsParticle.h
+++ include/hps_event/HpsParticle.h
@@ -1,57 +1,147 @@
 /**
- *
- * @author: 	Omar Moreno <omoreno1@ucsc.edu>
- * @section institution
- * 				Santa Cruz Institute for Particle Physics
- * 				University of California, Santa Cruz
- * @date:       March 29, 2013
- *
+ * @file: HpsParticle.h
+ * @author: Omar Moreno <omoreno1@ucsc.edu>
+ * @section Institution \n
+ * 			Santa Cruz Institute for Particle Physics
+ * 			University of California, Santa Cruz
+ * @date: March 29, 2013
  */
 
 #ifndef __HPS_PARTICLE_H__
 #define __HPS_PARTICLE_H__
 
+//------------//
 //--- ROOT ---//
 //------------//
 #include <TObject.h>
 #include <TClonesArray.h>
 #include <TRef.h>
+#include <TRefArray.h>
 
-//--- HPS Event ---//
-//-----------------//
-#include <SvtTrack.h>
-#include <EcalCluster.h>
+
+// Forward declare HPS event classes
+class SvtTrack;
+class EcalCluster;
 
 class HpsParticle : public TObject { 
 
 	public:
 
+        /**
+         * Default Constructor
+         */
 		HpsParticle(); 
-		HpsParticle(const HpsParticle &);
-		~HpsParticle();
+
+        /**
+         * Copy Constructor
+         *
+         * @param particle_obj : HpsParticle that will be cloned
+         */
+		HpsParticle(const HpsParticle &particle_obj);
+		
+        /**
+         * Destructor
+         */
+        ~HpsParticle();
+
+        /**
+         *
+         */
 		HpsParticle &operator=(const HpsParticle &);
 
+        /**
+         *
+         */
 		void Clear(Option_t *option="");
 
+        /**
+         *
+         */
 		void addTrack(SvtTrack* svt_track);
+        
+        /**
+         *
+         */
 		void addCluster(EcalCluster* ecal_cluster);
+        
+        /**
+         *
+         */
 		void addParticle(HpsParticle* particle); 
-
+        
+        /**
+         *
+         */
         void setCharge(const int charge){ this->charge = charge; };
+        
+        /**
+         *
+         */
 	    void setPDG(const int pdg){ this->pdg = pdg; }; 
+        
+        /**
+         *
+         */
         void setEnergy(const double energy){ this->energy = energy; };
+        
+        /**
+         *
+         */
 		void setMass(const double mass){ this->mass = mass; }; 
+        
+        /**
+         *
+         */
 		void setVertexPosition(const float*);
+        
+        /**
+         *
+         */
         void setMomentum(const double*); 
-	
+        
+        /**
+         *
+         */
 		TRefArray* getTracks() const; 
+        
+        /**
+         *
+         */
 		TRefArray* getClusters() const;
+        
+        /**
+         *
+         */
 		TRefArray* getParticles() const; 
+        
+        /**
+         *
+         */
         int getCharge() const { return charge; };
+        
+        /**
+         *
+         */
         int getPDG() const { return pdg; }; 
+        
+        /**
+         *
+         */
 		double getEnergy() const { return energy; }; 
+        
+        /**
+         *
+         */
 		double Mass() const { return mass; }; 
+        
+        /**
+         *
+         */
         std::vector<double> getMomentum() const;  
+        
+        /**
+         *
+         */
         std::vector<double> getVertexPosition() const; 
 
 		ClassDef(HpsParticle, 1);
