01c3647de9fef32c29521692dbbe6a56e8ae144c
Omar Moreno <email@omarmoreno.net>
2015-05-20 20:44:24 -0700

    Add the vertex fit chi^2 to the DST.

--- include/hps_event/HpsParticle.h
+++ include/hps_event/HpsParticle.h
@@ -55,94 +55,147 @@ class HpsParticle : public TObject {
 		void Clear(Option_t *option="");
 
         /**
+         * Add a reference to an SVT track associated with this particle.
          *
+         * @param svt_track : SVT track associated with this particle
          */
 		void addTrack(SvtTrack* svt_track);
         
         /**
+         * Add a reference to an Ecal cluster associated with this particle.
          *
+         * @param ecal_cluster : Ecal cluster associated with this particle
          */
 		void addCluster(EcalCluster* ecal_cluster);
         
         /**
+         * Add a daughter particle composing this particle.
          *
+         * @param particle : Daughter particle composing this particle
          */
 		void addParticle(HpsParticle* particle); 
         
         /**
+         * Set the charge of the particle.
          *
+         * @param charge : Particle charge
          */
-        void setCharge(const int charge){ this->charge = charge; };
+        void setCharge(const int charge) { this->charge = charge; };
         
         /**
+         * Set the particle ID.
          *
+         * @param pdg : The particle ID
          */
-	    void setPDG(const int pdg){ this->pdg = pdg; }; 
+	    void setPDG(const int pdg) { this->pdg = pdg; }; 
         
         /**
+         * Set the energy of the particle in GeV.
          *
+         * @param energy : The particle energy
          */
-        void setEnergy(const double energy){ this->energy = energy; };
+        void setEnergy(const double energy) { this->energy = energy; };
         
         /**
+         * Set the invariant mass of the particle in GeV.
          *
+         * @param mass : The invariant mass of the particle
          */
-		void setMass(const double mass){ this->mass = mass; }; 
+		void setMass(const double mass) { this->mass = mass; }; 
+       
+        /**
+         * Set the momentum of the particle in GeV.
+         *
+         * @param momentum : The momentum of the particle
+         */
+        void setMomentum(const double* momentum); 
+
+        /**
+         * Set the vertex position of the particle.
+         *
+         * @param vtx_pos : The vertex position of the particle
+         */
+		void setVertexPosition(const float* vtx_pos);
+
+        /**
+         * Set the chi^2 of the vertex fit.
+         *
+         * @param vtx_fit_chi2 : The chi^2 of the vertex fit
+         */
+        void setVertexFitChi2(const double vtx_fit_chi2) { this->vtx_fit_chi2 = vtx_fit_chi2; };
         
         /**
+         * Get the SVT tracks associated with this particle.
          *
-         */
-		void setVertexPosition(const float*);
-        
-        /**
-         *
-         */
-        void setMomentum(const double*); 
-        
-        /**
-         *
+         * @return An array of references to the tracks associated with this
+         *         particle
          */
 		TRefArray* getTracks() const; 
         
         /**
+         * Get the Ecal clusters associated with this particle.
          *
+         * @return An array of references to the Ecal clusters associated
+         *         with this particle
          */
 		TRefArray* getClusters() const;
         
         /**
+         * Get the daughter particles composing this particle.
          *
+         * @return An array of references to the daughter particles associated
+         *         with this particle
          */
 		TRefArray* getParticles() const; 
         
         /**
+         * Get the charge of the particle.
          *
+         * @return The particle charge
          */
         int getCharge() const { return charge; };
         
         /**
+         * Get the particle ID.
          *
+         * @return The particle ID
          */
         int getPDG() const { return pdg; }; 
         
         /**
+         * Get the energy of the particle in GeV.
          *
+         * @return The particle energy in GeV
          */
 		double getEnergy() const { return energy; }; 
         
         /**
+         * Get the invariant mass of the particle in GeV.
          *
+         * @return The invariant mass of the particle in GeV
          */
 		double Mass() const { return mass; }; 
         
         /**
+         * Get the momentum of the particle in GeV.
          *
+         * @return The momentum of the particle
          */
         std::vector<double> getMomentum() const;  
         
         /**
+         * Get the vertex position of the particle.
          *
+         * @return The vertex position of the particle
          */
-        std::vector<double> getVertexPosition() const; 
+        std::vector<double> getVertexPosition() const;
+
+        /**
+         * Get the chi^2 of the vertex fit.
+         *
+         * @return The chi^2 of the vertex fit
+         */
+        double getVertexFitChi2() const { return vtx_fit_chi2; }; 
 
 		ClassDef(HpsParticle, 1);
 
@@ -162,6 +215,7 @@ class HpsParticle : public TObject {
 		double vtx_x;
 		double vtx_y;
 		double vtx_z;
+        double vtx_fit_chi2;
 		double energy; 
 		double mass; 
 
--- src/hps_event/HpsParticle.cxx
+++ src/hps_event/HpsParticle.cxx
@@ -1,10 +1,10 @@
 /**
- *
- * @author: 	Omar Moreno <omoreno1@ucsc.edu>
- * @section institution
- * 				Santa Cruz Institute for Particle Physics
- * 				University of California, Santa Cruz
- * @date:       March 29, 2013
+ * @file: HpsParticle.cxx
+ * @author: Omar Moreno <omoreno1@ucsc.edu>
+ * @section Institution \n
+ *          Santa Cruz Institute for Particle Physics
+ *          University of California, Santa Cruz
+ * @date: March 29, 2013
  */
 
 #include <HpsParticle.h>
@@ -12,10 +12,10 @@
 ClassImp(HpsParticle)
 
 HpsParticle::HpsParticle()
-	: TObject(),
+    : TObject(),
       svt_tracks(new TRefArray()),
       ecal_clusters(new TRefArray()),
-	  particles(new TRefArray()),
+      particles(new TRefArray()),
       n_daughters(0),
       charge(0), 
       px(0),
@@ -24,16 +24,17 @@ HpsParticle::HpsParticle()
       vtx_x(0),
       vtx_y(0),
       vtx_z(0),
+      vtx_fit_chi2(0),
       energy(0),
       mass(0) {
 }
 
 HpsParticle::HpsParticle(const HpsParticle &particle_obj)
-	: TObject(),
+    : TObject(),
       svt_tracks(new TRefArray()),
       ecal_clusters(new TRefArray()),
-	  particles(new TRefArray()),
-	  n_daughters(particle_obj.n_daughters),
+      particles(new TRefArray()),
+      n_daughters(particle_obj.n_daughters),
       charge(particle_obj.charge),
       px(particle_obj.px),
       py(particle_obj.py),
@@ -41,69 +42,71 @@ HpsParticle::HpsParticle(const HpsParticle &particle_obj)
       vtx_x(particle_obj.vtx_x),
       vtx_y(particle_obj.vtx_y),
       vtx_z(particle_obj.vtx_z),
-	  energy(particle_obj.energy),
+      vtx_fit_chi2(particle_obj.vtx_fit_chi2),
+      energy(particle_obj.energy),
       mass(particle_obj.mass) {
 
-	*svt_tracks = *particle_obj.svt_tracks;
-	*ecal_clusters = *particle_obj.ecal_clusters;	
-	*particles = *particle_obj.particles; 
+    *svt_tracks = *particle_obj.svt_tracks;
+    *ecal_clusters = *particle_obj.ecal_clusters;   
+    *particles = *particle_obj.particles; 
 }
 
 HpsParticle::~HpsParticle() {
-	Clear();
+    Clear();
     delete svt_tracks; 
-	delete ecal_clusters; 
+    delete ecal_clusters; 
     delete particles;
 }
 
 HpsParticle &HpsParticle::operator=(const HpsParticle &particle_obj) {
-	// Check for self-assignment
-	if(this == &particle_obj) return *this;
+    // Check for self-assignment
+    if(this == &particle_obj) return *this;
 
-	TObject::operator=(particle_obj);
-	Clear(); 
+    TObject::operator=(particle_obj);
+    Clear(); 
 
-	this->n_daughters = particle_obj.n_daughters;
+    this->n_daughters = particle_obj.n_daughters;
     this->charge = particle_obj.charge;
 
     this->px = particle_obj.px; 
     this->py = particle_obj.py; 
     this->pz = particle_obj.pz; 
-	this->vtx_x = particle_obj.vtx_x;
-	this->vtx_y = particle_obj.vtx_y;
-	this->vtx_z = particle_obj.vtx_z;
-	this->energy = particle_obj.energy;
-	this->mass = particle_obj.mass; 
+    this->vtx_x = particle_obj.vtx_x;
+    this->vtx_y = particle_obj.vtx_y;
+    this->vtx_z = particle_obj.vtx_z;
+    this->vtx_fit_chi2 = particle_obj.vtx_fit_chi2;
+    this->energy = particle_obj.energy;
+    this->mass = particle_obj.mass; 
 
-	svt_tracks = new TRefArray();
-	*svt_tracks = *particle_obj.svt_tracks;
-	ecal_clusters = new TRefArray(); 
-	*ecal_clusters = *particle_obj.ecal_clusters;
-	particles = new TRefArray(); 
-	*particles = *particle_obj.particles; 	
+    svt_tracks = new TRefArray();
+    *svt_tracks = *particle_obj.svt_tracks;
+    ecal_clusters = new TRefArray(); 
+    *ecal_clusters = *particle_obj.ecal_clusters;
+    particles = new TRefArray(); 
+    *particles = *particle_obj.particles;   
 
-	return *this; 
+    return *this; 
 }
 
 void HpsParticle::Clear(Option_t* /* option */) {
-	TObject::Clear();
-	svt_tracks->Delete();
-	ecal_clusters->Delete(); 
+    TObject::Clear();
+    svt_tracks->Delete();
+    ecal_clusters->Delete(); 
     particles->Delete();     
-	n_daughters = 0; 	
+    n_daughters = 0;    
 }
 
 void HpsParticle::addTrack(SvtTrack* svt_track) {
-	svt_tracks->Add((TObject*) svt_track);
+    svt_tracks->Add((TObject*) svt_track);
 }
 
 void HpsParticle::addCluster(EcalCluster* ecal_cluster) {
-	ecal_clusters->Add((TObject*) ecal_cluster);
+    ecal_clusters->Add((TObject*) ecal_cluster);
 }
 
 void HpsParticle::addParticle(HpsParticle* particle) {
-	++n_daughters;
-	particles->Add(particle); 
+    ++n_daughters;
+    particles->Add(particle); 
 }
 
 void HpsParticle::setMomentum(const double* momentum) {
@@ -113,21 +116,21 @@ void HpsParticle::setMomentum(const double* momentum) {
 }
 
 void HpsParticle::setVertexPosition(const float* vtx_pos) {
-	vtx_x = (double) vtx_pos[0];
-	vtx_y = (double) vtx_pos[1];
-	vtx_z = (double) vtx_pos[2];
+    vtx_x = (double) vtx_pos[0];
+    vtx_y = (double) vtx_pos[1];
+    vtx_z = (double) vtx_pos[2];
 }
 
 TRefArray* HpsParticle::getTracks() const {
-	return svt_tracks;
+    return svt_tracks;
 }
 
 TRefArray* HpsParticle::getClusters() const {
-	return ecal_clusters;
+    return ecal_clusters;
 }
 
 TRefArray* HpsParticle::getParticles() const {
-	return particles; 
+    return particles; 
 }
 
 std::vector<double> HpsParticle::getMomentum() const {
