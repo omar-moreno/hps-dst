2ed18835a1153ce578213dc26fba0349dfbe6374
Omar Moreno <email@omarmoreno.net>
2015-05-21 00:17:15 -0700

    Add the Ecal cluster time to the DST.  This is currently set to the hit time of the cluster seed.  Remove the shower moments from the DST until they are added to the recon.  Added some documentation.

--- include/hps_event/EcalCluster.h
+++ include/hps_event/EcalCluster.h
@@ -1,20 +1,21 @@
 /**
- *
- * @author: 	Omar Moreno <omoreno1@ucsc.edu>
- * @section institution
- * 				Santa Cruz Institute for Particle Physics
- * 				University of California, Santa Cruz
- * @version:    v 1.0
- * @date:       February 19, 2013
+ * @file: EcalCluster.h
+ * @author: Omar Moreno <omoreno1@ucsc.edu>
+ * @section Institution \n
+ *          Santa Cruz Institute for Particle Physics
+ *          University of California, Santa Cruz
+ * @date: February 19, 2013
  */
 
-#ifndef _ECAL_CLUSTER_H_
-#define _ECAL_CLUSTER_H_
+#ifndef __ECAL_CLUSTER_H__
+#define __ECAL_CLUSTER_H__
 
-//--- C++ ---//
-//-----------//
+//------------------//
+//--- C++ StdLib ---//
+//------------------//
 #include <iostream>
 
+//-----------//
 //-- ROOT ---//
 //-----------//
 #include <TObject.h>
@@ -22,6 +23,7 @@
 #include <TRefArray.h>
 #include <TRef.h>
 
+//-----------------//
 //--- HPS Event ---//
 //-----------------//
 #include <Cluster.h>
@@ -31,27 +33,106 @@ class EcalCluster : public TObject, public Cluster {
 
     public:
 
+        /**
+         * Default Destructor
+         */
         EcalCluster();
-        EcalCluster(const EcalCluster &);
+        
+        /**
+         * Copy Constructor
+         *
+         * @param ecal_cluster_obj : EcalCluster that will be cloned 
+         */
+        EcalCluster(const EcalCluster &ecal_cluster_obj);
+
+        /**
+         * Destructor
+         */
         ~EcalCluster();
+
+        /**
+         *
+         */
         EcalCluster &operator=(const EcalCluster &);
 
+        /**
+         *
+         */
         void Clear(Option_t *option="");
 
-		void addHit(EcalHit*); 
+        /**
+         * Add a reference to an Ecal hit composing with this cluster.
+         *
+         * @param hit : Ecal hit composing with this cluster
+         */
+		void addHit(EcalHit* hit); 
 
-        void setPosition(const float*);
-        void setEnergy(const double energy){ this->energy = energy; };
-        void setHitTime(const double hit_time){ this->hit_time = hit_time; };
-        void setM2(const double m2){ this->m2 = m2; };
-        void setM3(const double m3){ this->m3 = m3; };
+        /**
+         * Set the position of the Ecal cluster.
+         *
+         * @param position : The position of the Ecal cluster
+         */
+        void setPosition(const float* position);
+        
+        /**
+         * Set the energy of the Ecal cluster.
+         *
+         * @param energy : The energy of the Ecal cluster.
+         */
+        void setEnergy(const double energy) { this->energy = energy; };
+        
+        /**
+         * Set the time of the cluster.  This currently comes from the hit time
+         * of the seed hit.
+         *
+         * @param cluster_time : The time of the cluster
+         */
+        //void setClusterTime(const double cluster_time) { this->cluster_time = cluster_time; };
+        
+        // These will be commented out until they are part of the variables
+        // in the reconstruction.
+        //void setM2(const double m2){ this->m2 = m2; };
+        //void setM3(const double m3){ this->m3 = m3; };
 
+        /**
+         * Get the position of the Ecal cluster.
+         *
+         * @return The position of the Ecal cluster
+         */
 		std::vector<double> getPosition() const;  
+        
+        /**
+         * Get the energy of the Ecal cluster.
+         *
+         * @return The energy of the Ecal cluster.
+         */
         double getEnergy() const { return energy; };
+       
+        /**
+         * Get the time of the cluster.  This currently comes from the hit time
+         * of the seed hit.
+         *
+         * @return The time of the cluster
+         */
+        double getClusterTime() const { return cluster_time; };
+        
+
+        /**
+         * Get the seed hit of the cluster.
+         *
+         * @return The seed hit of the cluster
+         */
         EcalHit* getSeed() const;
+        
+        /**
+         * Get the Ecal hits composing this cluster.
+         *
+         * @return An array of references to the Ecal hits composing this cluster
+         */
         TRefArray* getEcalHits() const;
-		double getM2() const { return m2; };
-        double getM3() const { return m3; };
+		
+        //double getM2() const { return m2; };
+        //double getM3() const { return m3; };
 
         ClassDef(EcalCluster, 1);	
 
@@ -66,9 +147,10 @@ class EcalCluster : public TObject, public Cluster {
         double y; 
         double z;
         double energy; 
-        double hit_time;
-        double m2;
-        double m3;
+        double cluster_time;
+        
+        //double m2;
+        //double m3;
 
 }; // EcalCluster
 
