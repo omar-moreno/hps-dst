0475a2dd89b15bb344e4d2b11e988c90ffdd4cb0
Omar Moreno <email@omarmoreno.net>
2015-08-05 23:36:48 -0700

    For now, revert changes to GblTrack so it can be used in analysis.

--- include/hps_event/GblTrack.h
+++ include/hps_event/GblTrack.h
@@ -1,198 +1,74 @@
 /**
- * @file: GblTrack.h
- * @author: Per Hansson <phansson@slac.stanford.edu>
- * @section Institutiion \n
- *          SLAC
- * @author: Omar Moreno <omoreno1@ucsc.edu>
- * @section Institution \n
- *          Santa Cruz Institute for Particle Physics
- *          University of California, Santa Cruz
- * @date: February 3, 2014
+ *
+ * @author: 	phansson@slac.stanford.edu
+ * @section purpose: GBL track information
+ * @version:    v1.0
+ * @date:       February 3, 2014
  */
 
-#ifndef __GBL_TRACK_H__
-#define __GBL_TRACK_H__
+#ifndef _GBL_TRACK_H_
+#define _GBL_TRACK_H_
 
-//------------------//
-//--- C++ StdLib ---//
-//------------------//
+//--- C++ ---//
 #include <iostream>
 
-//------------//
 //--- ROOT ---//
-//------------//
 #include <TObject.h>
 #include <TClonesArray.h>
 #include <TRefArray.h>
 #include <TMatrixD.h>
-#include <TRef.h>
-
-// Forward declarations
-class SvtTrack;
 
 class GblTrack : public TObject {
+  
+ public:
+  GblTrack();
+  virtual ~GblTrack();
 
-    public:
+   // setters
+   void setSeedTrackParameters(double kappa, double theta, double phi, double d0, double z0);
+   void setTrackParameters(double C, double th, double phi0, double dca, double z);
+   void setChi2(double c);
+   void setNdf(double ndof);
+   void setMomentumVector(double x, double y, double z);
+   void setCov(const TMatrixD& mat);
 
-        /**
-         * Default Constructor
-         */
-        GblTrack();
+   // Getters
+   double getPx()  	  const { return px; };
+   double getPy()  	  const { return py; };
+   double getPz()  	  const { return pz; };
+   double getKappa() const { return kappa;}
+   double getTheta() const { return theta;}
+   double getPhi() const { return phi;}
+   double getD0() const { return d0;}
+   double getZ0() const { return z0;}
+   double getChi2() const { return chi2;}
+   double getNdf() const { return ndf;}
+   double getSeedKappa() const { return seed_kappa;}
+   double getSeedTheta() const { return seed_theta;}
+   double getSeedPhi() const { return seed_phi;}
+   double getSeedD0() const { return seed_d0; }
+   double getSeedZ0() const { return seed_z0; }
+   void print();
 
-        /**
-         * Destructor
-         */
-        ~GblTrack();
-
-        void setSeedTrackParameters(double kappa, double theta, double phi, double d0, double z0);
-        
-        /**
-         * Set the track parameters.
-         *
-         * @param d0 : Distance of closest approach
-         * @param phi0 : 
-         * @param kappa : 
-         * @param theta : 
-         * @param z0 : 
-         */
-        void setTrackParameters(double C, double th, double phi0, double dca, double z);
-        
-        /**
-         *
-         */
-        void setChi2(double c);
-        
-        /**
-         *
-         */
-        void setNdf(double ndof);
-        
-        /**
-         *
-         */
-        void setMomentumVector(double x, double y, double z);
-        
-        /**
-         *
-         */
-        void setCov(const TMatrixD& mat);
-
-        /**
-         * Set the seed track associated with this GBL track.
-         *
-         * @param track : seed track associated with this GBL track
-         */
-        void setSeedTrack(SvtTrack* seed_track) { this->seed_track = (TObject*) seed_track; }; 
-
-        /**
-         *
-         */
-        double getPx()  	  const { return px; };
-        
-        /**
-         *
-         */
-        double getPy()  	  const { return py; };
-        
-        /**
-         *
-         */
-        double getPz()  	  const { return pz; };
-        
-        /**
-         *
-         */
-        double getKappa() const { return kappa;}
-        
-        /**
-         *
-         */
-        double getTheta() const { return theta;}
-        
-        /**
-         *
-         */
-        double getPhi() const { return phi;}
-        
-        /**
-         *
-         */
-        double getD0() const { return d0;}
-        
-        /**
-         *
-         */
-        double getZ0() const { return z0;}
-        
-        /**
-         *
-         */
-        double getChi2() const { return chi2;}
-        
-        /**
-         *
-         */
-        double getNdf() const { return ndf;}
-        
-        /**
-         *
-         */
-        double getSeedKappa() const { return seed_kappa;}
-        
-        /**
-         *
-         */
-        double getSeedTheta() const { return seed_theta;}
-        
-        /**
-         *
-         */
-        double getSeedPhi() const { return seed_phi;}
-        
-        /**
-         *
-         */
-        double getSeedD0() const { return seed_d0; }
-        
-        /**
-         *
-         */
-        double getSeedZ0() const { return seed_z0; }
-
-        /**
-         * Get the seed track associated with this GBL track.
-         *
-         * @return seed track associated with this GBL track
-         */
-        TRef getSeedTrack() const { return seed_track; }; 
-
-        /**
-         *
-         */
-        void print();
-
-        ClassDef(GblTrack,1) //Track class for use with GBL
-
-    private:
-            
-            TRef seed_track;
-
-            double kappa;
-            double theta;
-            double phi;
-            double d0;
-            double z0;
-            double seed_kappa;
-            double seed_theta;
-            double seed_phi;
-            double seed_d0;
-            double seed_z0;
-            double chi2;
-            double ndf;
-            double px;
-            double py;
-            double pz;
-            TMatrixD cov;
+   ClassDef(GblTrack,1) //Track class for use with GBL
+   
+ private:
+   double kappa;
+   double theta;
+   double phi;
+   double d0;
+   double z0;
+   double seed_kappa;
+   double seed_theta;
+   double seed_phi;
+   double seed_d0;
+   double seed_z0;
+   double chi2;
+   double ndf;
+   double px;
+   double py;
+   double pz;
+   TMatrixD cov;
 
 }; // GblTrack
 
--- src/hps_event/GblTrack.cxx
+++ src/hps_event/GblTrack.cxx
@@ -1,27 +1,24 @@
 /**
- * @file: GblTrack.h
- * @author: Per Hansson <phansson@slac.stanford.edu>
- * @section Institutiion \n
- *          SLAC
- * @author: Omar Moreno <omoreno1@ucsc.edu>
- * @section Institution \n
- *          Santa Cruz Institute for Particle Physics
- *          University of California, Santa Cruz
- * @date: February 3, 2014
+ *
+ * @author: 	phansson@slac.stanford.edu
+ * @section purpose: GBL track information
+ * @version:    v1.0
+ * @date:       February 3, 2014
  */
 
+//-- HPS EVENT --//
 #include <GblTrack.h>
+
+using namespace std;
 
 ClassImp(GblTrack)
 
-GblTrack::GblTrack() 
-    : TObject(),
-      chi2(-1.),
-      cov(5,5) {
-}
+GblTrack::GblTrack() : TObject(), chi2(-1.), cov(5,5)
+{}
 
 
-GblTrack::~GblTrack() {
+GblTrack::~GblTrack()
+{
 }
 
 void GblTrack::setSeedTrackParameters(double kappa, double theta, double phi, double d0, double z0) {
@@ -41,26 +38,26 @@ void GblTrack::setTrackParameters(double C, double th, double phi0, double dca, 
 }
 
 void GblTrack::setCov(const TMatrixD& mat) {
-    cov = mat;
+  cov = mat;
 }
 
 void GblTrack::setChi2(double c) {
-    chi2 = c;
+chi2 = c;
 }
 
 void GblTrack::setNdf(double ndof) {
-    ndf = ndof;
+   ndf = ndof;
 }
 
 void GblTrack::setMomentumVector(double x, double y, double z) {
-    px = x;
-    py = y;
-    pz = z;
+   px = x;
+   py = y;
+   pz = z;
 }
 
 void GblTrack::print() {
-    std::cout << "GblTrack: " << "\n";
-    std::cout << "seed        (kappa,theta,phi,d0,z0): " << getKappa() << "," << getTheta() << "," << getPhi() << "," << getD0() << "," << getZ0() << "\n";
-    std::cout << "seed params (kappa,theta,phi,d0,z0): " << getSeedKappa() << "," << getSeedTheta() << "," << getSeedPhi() << "," << getSeedD0() << "," << getSeedZ0() << "\n";
-    std::cout << std::endl;
-}
+     cout << "GblTrack: " << "\n";
+     cout << "seed        (kappa,theta,phi,d0,z0): " << getKappa() << "," << getTheta() << "," << getPhi() << "," << getD0() << "," << getZ0() << "\n";
+     cout << "seed params (kappa,theta,phi,d0,z0): " << getSeedKappa() << "," << getSeedTheta() << "," << getSeedPhi() << "," << getSeedD0() << "," << getSeedZ0() << "\n";
+     cout << endl;
+   }
