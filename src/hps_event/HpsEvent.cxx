c4a7dcd9ef452ec3f4a3b4823951fb442ff87488
Omar Moreno <email@omarmoreno.net>
2015-09-09 11:56:48 -0700

    Format before making changes.

--- src/hps_event/HpsEvent.cxx
+++ src/hps_event/HpsEvent.cxx
@@ -1,68 +1,71 @@
 /**
  *
- * @author: 	Omar Moreno <omoreno1@ucsc.edu>
- * @section institution
- * 				Santa Cruz Institute for Particle Physics
- * 				University of California, Santa Cruz
- * @date:       February 19, 2013
+ * @file HpsEvent.cxx
+ * @brief Event class used to encapsulate event information and physics 
+ *        collections.
+ * @author Omar Moreno <omoreno1@ucsc.edu>
+ *         Santa Cruz Institute for Particle Physics
+ *         University of California, Santa Cruz
+ * @date February 19, 2013
+ *
  */
 #include <HpsEvent.h>
 
 ClassImp(HpsEvent)
-	
+
 HpsEvent::HpsEvent()
-    :   TObject(), 
-        tracks(new TClonesArray("SvtTrack", 1000)),
-        svt_hits(new TClonesArray("SvtHit", 1000)),
-        ecal_clusters(new TClonesArray("EcalCluster", 1000)),
-        ecal_hits(new TClonesArray("EcalHit", 1000)),
-        fs_particles(new TClonesArray("HpsParticle", 1000)),
-        uc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
-        uc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
-        bsc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
-        bsc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
-        tc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
-        tc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
-        mc_particles(new TClonesArray("HpsMCParticle", 1000)),
-        gbl_tracks(new TClonesArray("GblTrack", 1000)),
-        gbl_tracks_data(new TClonesArray("GblTrackData", 1000)),
-        gbl_strips_data(new TClonesArray("GblStripData", 1000)),
-        event_number(0),
-        run_number(0),
-        n_tracks(0),
-        n_svt_hits(0),
-        n_ecal_clusters(0), 
-        n_ecal_hits(0), 
-        n_fs_particles(0),
-        n_uc_v0_candidates(0), 
-        n_uc_moller_candidates(0), 
-        n_bsc_v0_candidates(0),
-        n_bsc_moller_candidates(0),
-        n_tc_v0_candidates(0), 
-        n_tc_moller_candidates(0), 
-        n_mc_particles(0),
-        n_gbl_tracks_data(0),
-        n_gbl_strips_data(0)
-{}
+    : TObject(), 
+      tracks(new TClonesArray("SvtTrack", 1000)),
+      svt_hits(new TClonesArray("SvtHit", 1000)),
+      ecal_clusters(new TClonesArray("EcalCluster", 1000)),
+      ecal_hits(new TClonesArray("EcalHit", 1000)),
+      fs_particles(new TClonesArray("HpsParticle", 1000)),
+      uc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
+      uc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
+      bsc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
+      bsc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
+      tc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
+      tc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
+      mc_particles(new TClonesArray("HpsMCParticle", 1000)),
+      gbl_tracks(new TClonesArray("GblTrack", 1000)),
+      gbl_tracks_data(new TClonesArray("GblTrackData", 1000)),
+      gbl_strips_data(new TClonesArray("GblStripData", 1000)),
+      event_number(0),
+      run_number(0),
+      n_tracks(0),
+      n_svt_hits(0),
+      n_ecal_clusters(0), 
+      n_ecal_hits(0), 
+      n_fs_particles(0),
+      n_uc_v0_candidates(0), 
+      n_uc_moller_candidates(0), 
+      n_bsc_v0_candidates(0),
+      n_bsc_moller_candidates(0),
+      n_tc_v0_candidates(0), 
+      n_tc_moller_candidates(0), 
+      n_mc_particles(0),
+      n_gbl_tracks_data(0),
+      n_gbl_strips_data(0) {
+}
 
 HpsEvent::HpsEvent(const HpsEvent &hpsEventObj)
-    :   TObject(hpsEventObj),
-        tracks(new TClonesArray("SvtTrack", 1000)),
-        svt_hits(new TClonesArray("SvtHit", 1000)),
-        ecal_clusters(new TClonesArray("EcalCluster", 1000)),
-        ecal_hits(new TClonesArray("EcalHit", 1000)),
-        fs_particles(new TClonesArray("HpsParticle", 1000)),
-        uc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
-        uc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
-        bsc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
-        bsc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
-        tc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
-        tc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
-        mc_particles(new TClonesArray("HpsMCParticle", 1000)),
-        gbl_tracks(new TClonesArray("GblTrack", 1000)),
-        gbl_tracks_data(new TClonesArray("GblTrackData", 1000)),
-        gbl_strips_data(new TClonesArray("GblStripData", 1000))
-{
+    : TObject(hpsEventObj),
+      tracks(new TClonesArray("SvtTrack", 1000)),
+      svt_hits(new TClonesArray("SvtHit", 1000)),
+      ecal_clusters(new TClonesArray("EcalCluster", 1000)),
+      ecal_hits(new TClonesArray("EcalHit", 1000)),
+      fs_particles(new TClonesArray("HpsParticle", 1000)),
+      uc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
+      uc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
+      bsc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
+      bsc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
+      tc_v0_candidates(new TClonesArray("HpsParticle", 1000)),
+      tc_moller_candidates(new TClonesArray("HpsParticle", 1000)), 
+      mc_particles(new TClonesArray("HpsMCParticle", 1000)),
+      gbl_tracks(new TClonesArray("GblTrack", 1000)),
+      gbl_tracks_data(new TClonesArray("GblTrackData", 1000)),
+      gbl_strips_data(new TClonesArray("GblStripData", 1000)) {
+
     this->event_number = hpsEventObj.event_number; 
     this->run_number   = hpsEventObj.run_number; 
     this->n_tracks     = hpsEventObj.n_tracks; 
@@ -99,9 +102,8 @@ HpsEvent::HpsEvent(const HpsEvent &hpsEventObj)
 }
 
 
-HpsEvent::~HpsEvent()
-{
-	Clear();
+HpsEvent::~HpsEvent() {
+    Clear();
     delete tracks; 
     delete svt_hits;
     delete ecal_clusters; 
@@ -119,11 +121,10 @@ HpsEvent::~HpsEvent()
     delete gbl_strips_data;
 }
 
-HpsEvent &HpsEvent::operator=(const HpsEvent &hpsEventObj)
-{
+HpsEvent &HpsEvent::operator=(const HpsEvent &hpsEventObj) {
     // Check for self-assignment
-    if(this == &hpsEventObj) return *this;
-    
+    if (this == &hpsEventObj) return *this;
+
     TObject::operator=(hpsEventObj);
     this->~HpsEvent();
     this->event_number = hpsEventObj.event_number; 
@@ -179,8 +180,7 @@ HpsEvent &HpsEvent::operator=(const HpsEvent &hpsEventObj)
     return *this;     
 }
 
-void HpsEvent::Clear(Option_t * /*option*/)
-{
+void HpsEvent::Clear(Option_t * /*option*/) {
     TObject::Clear(); 
     tracks->Clear("C");
     svt_hits->Clear("C");
@@ -215,134 +215,123 @@ void HpsEvent::Clear(Option_t * /*option*/)
 }
 
 
-SvtTrack* HpsEvent::addTrack()
-{
-	return (SvtTrack*) tracks->ConstructedAt(n_tracks++);
+SvtTrack* HpsEvent::addTrack() {
+    return (SvtTrack*) tracks->ConstructedAt(n_tracks++);
 }
 
-SvtHit* HpsEvent::addSvtHit()
-{
+SvtHit* HpsEvent::addSvtHit() {
     return (SvtHit*) svt_hits->ConstructedAt(n_svt_hits++);
 }
 
-EcalCluster* HpsEvent::addEcalCluster()
-{
+EcalCluster* HpsEvent::addEcalCluster() {
     return (EcalCluster*) ecal_clusters->ConstructedAt(n_ecal_clusters++);
 }
 
-EcalHit* HpsEvent::addEcalHit()
-{
-	return (EcalHit*) ecal_hits->ConstructedAt(n_ecal_hits++);
+EcalHit* HpsEvent::addEcalHit() {
+    return (EcalHit*) ecal_hits->ConstructedAt(n_ecal_hits++);
 }
 
-HpsParticle* HpsEvent::addParticle(HpsParticle::particle_type type)
-{
-	switch(type) { 
+HpsParticle* HpsEvent::addParticle(HpsParticle::particle_type type) {
+    switch(type) { 
         case HpsParticle::FINAL_STATE_PARTICLE:
             return (HpsParticle*) fs_particles->ConstructedAt(n_fs_particles++); 
-		case HpsParticle::UC_V0_CANDIDATE: 
-			return (HpsParticle*) uc_v0_candidates->ConstructedAt(n_uc_v0_candidates++);
-		case HpsParticle::BSC_V0_CANDIDATE:
-			return (HpsParticle*) bsc_v0_candidates->ConstructedAt(n_bsc_v0_candidates++);
-		case HpsParticle::TC_V0_CANDIDATE:
-			return (HpsParticle*) tc_v0_candidates->ConstructedAt(n_tc_v0_candidates++);
-		default: 
-			// TODO: If a collection type is invalid, throw an exception instead
-			return NULL; 
-	}
+        case HpsParticle::UC_V0_CANDIDATE: 
+            return (HpsParticle*) uc_v0_candidates->ConstructedAt(n_uc_v0_candidates++);
+        case HpsParticle::BSC_V0_CANDIDATE:
+            return (HpsParticle*) bsc_v0_candidates->ConstructedAt(n_bsc_v0_candidates++);
+        case HpsParticle::TC_V0_CANDIDATE:
+            return (HpsParticle*) tc_v0_candidates->ConstructedAt(n_tc_v0_candidates++);
+        default: 
+            // TODO: If a collection type is invalid, throw an exception instead
+            return NULL; 
+    }
 }
 
-HpsMCParticle* HpsEvent::addHpsMCParticle()
-{
-	return (HpsMCParticle*) mc_particles->ConstructedAt(n_mc_particles++);
+HpsMCParticle* HpsEvent::addHpsMCParticle() {
+    return (HpsMCParticle*) mc_particles->ConstructedAt(n_mc_particles++);
 }
 
 
-int HpsEvent::getNumberOfParticles(HpsParticle::particle_type type) const
-{
-	switch(type){
-		case HpsParticle::FINAL_STATE_PARTICLE:
-			return n_fs_particles;
-		case HpsParticle::UC_V0_CANDIDATE: 
-			return n_uc_v0_candidates;  
-		case HpsParticle::BSC_V0_CANDIDATE:
-			return n_bsc_v0_candidates;
-		case HpsParticle::TC_V0_CANDIDATE:
-			return n_tc_v0_candidates; 
-		default: 
-			// TODO: If a collection type is invalid, throw an exception instead
-			return -1; 
-	}
-}
-
-GblTrack* HpsEvent::addGblTrack()
-{
-	return (GblTrack*) gbl_tracks->ConstructedAt(n_gbl_tracks++);
-}
-
-GblTrackData* HpsEvent::addGblTrackData()
-{
-	return (GblTrackData*) gbl_tracks_data->ConstructedAt(n_gbl_tracks_data++);
-}
-
-GblStripData* HpsEvent::addGblStripData()
-{
-	return (GblStripData*) gbl_strips_data->ConstructedAt(n_gbl_strips_data++);
-}
-
-SvtTrack* HpsEvent::getTrack(int track_n)
-{
-	return (SvtTrack*) tracks->At(track_n);
-}
-
-GblTrack* HpsEvent::getGblTrack(int track_n)
-{
-	return (GblTrack*) gbl_tracks->At(track_n);
-}
-
-GblTrackData* HpsEvent::getGblTrackData(int track_n)
-{
-	return (GblTrackData*) gbl_tracks_data->At(track_n);
-}
-
-GblStripData* HpsEvent::getGblStripData(int strip_n)
-{
-	return (GblStripData*) gbl_strips_data->At(strip_n);
-}
-
-SvtHit* HpsEvent::getSvtHit(int hit_n)
-{
-	return (SvtHit*) svt_hits->At(hit_n);
-}
-
-EcalCluster* HpsEvent::getEcalCluster(int ecal_cluster_n)
-{
-	return (EcalCluster*) ecal_clusters->At(ecal_cluster_n);
-}
-
-EcalHit* HpsEvent::getEcalHit(int ecal_hit_n)
-{
-	return (EcalHit*) ecal_hits->At(ecal_hit_n);
-}
-
-HpsMCParticle* HpsEvent::getMCParticle(int mc_particle_n)
-{
-	return (HpsMCParticle*) mc_particles->At(mc_particle_n);
-}
-
-HpsParticle* HpsEvent::getParticle(HpsParticle::particle_type type, int particle_n)
-{
-	switch(type){ 
+int HpsEvent::getNumberOfParticles(HpsParticle::particle_type type) {
+    switch(type){
         case HpsParticle::FINAL_STATE_PARTICLE:
-            return (HpsParticle*) fs_particles->At(particle_n); 
-		case HpsParticle::UC_V0_CANDIDATE: 
-			return (HpsParticle*) uc_v0_candidates->At(particle_n);
-		case HpsParticle::BSC_V0_CANDIDATE: 
-			return (HpsParticle*) bsc_v0_candidates->At(particle_n);
-		case HpsParticle::TC_V0_CANDIDATE: 
-			return (HpsParticle*) tc_v0_candidates->ConstructedAt(particle_n);
-		default:
-			// TODO: If the collection type is invalid, throw an exception instead.	
-			return NULL; 
-	}
+            return n_fs_particles;
+        case HpsParticle::UC_V0_CANDIDATE: 
+            return n_uc_v0_candidates;  
+        case HpsParticle::BSC_V0_CANDIDATE:
+            return n_bsc_v0_candidates;
+        case HpsParticle::TC_V0_CANDIDATE:
+            return n_tc_v0_candidates; 
+        default: 
+            // TODO: If a collection type is invalid, throw an exception instead
+            return -1; 
+    }
+}
+
+GblTrack* HpsEvent::addGblTrack() {
+    return (GblTrack*) gbl_tracks->ConstructedAt(n_gbl_tracks++);
+}
+
+GblTrackData* HpsEvent::addGblTrackData() {
+    return (GblTrackData*) gbl_tracks_data->ConstructedAt(n_gbl_tracks_data++);
+}
+
+GblStripData* HpsEvent::addGblStripData() {
+    return (GblStripData*) gbl_strips_data->ConstructedAt(n_gbl_strips_data++);
+}
+
+SvtTrack* HpsEvent::getTrack(int track_index) {
+    return (SvtTrack*) tracks->At(track_index);
+}
+
+GblTrack* HpsEvent::getGblTrack(int gbl_track_index) {
+    return (GblTrack*) gbl_tracks->At(gbl_track_index);
+}
+
+GblTrackData* HpsEvent::getGblTrackData(int track_data_index) {
+    return (GblTrackData*) gbl_tracks_data->At(track_data_index);
+}
+
+GblStripData* HpsEvent::getGblStripData(int gbl_strip_data_index) {
+    return (GblStripData*) gbl_strips_data->At(gbl_strip_data_index);
+}
+
+SvtHit* HpsEvent::getSvtHit(int hit_index) {
+    return (SvtHit*) svt_hits->At(hit_index);
+}
+
+EcalCluster* HpsEvent::getEcalCluster(int ecal_cluster_index) {
+    return (EcalCluster*) ecal_clusters->At(ecal_cluster_index);
+}
+
+EcalHit* HpsEvent::getEcalHit(int ecal_hit_index) {
+    return (EcalHit*) ecal_hits->At(ecal_hit_index);
+}
+
+HpsMCParticle* HpsEvent::getMCParticle(int mc_particle_index) {
+    return (HpsMCParticle*) mc_particles->At(mc_particle_n);
+}
+
+HpsParticle* HpsEvent::getParticle(HpsParticle::particle_type type, int particle_index) {
+    
+    // FIXME: This should probably be using a map instead of a giant switch 
+    //        statement.
+    switch(type) { 
+        case HpsParticle::FINAL_STATE_PARTICLE:
+            return (HpsParticle*) fs_particles->At(particle_index); 
+        case HpsParticle::UC_V0_CANDIDATE: 
+            return (HpsParticle*) uc_v0_candidates->At(particle_index);
+        case HpsParticle::BSC_V0_CANDIDATE: 
+            return (HpsParticle*) bsc_v0_candidates->At(particle_index);
+        case HpsParticle::TC_V0_CANDIDATE: 
+            return (HpsParticle*) tc_v0_candidates->ConstructedAt(particle_index);
+        case HpsParticle::UC_MOLLER_CANDIDATE: 
+            return (HpsParticle*) uc_moller_candidates->At(particle_index);
+        case HpsParticle::BSC_MOLLER_CANDIDATE: 
+            return (HpsParticle*) bsc_moller_candidates->At(particle_index);
+        case HpsParticle::TC_MOLLER_CANDIDATE: 
+            return (HpsParticle*) tc_moller_candidates->ConstructedAt(particle_index);
+        default:
+            return throw std::runtime_error("[ HpsEvent ]: Particle type is invalid."); 
+    }
 }
