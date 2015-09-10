7b452f3a64858477d47b7f7ebb5c6c07b6f646b3
Omar Moreno <omoreno1@ucsc.edu>
2015-09-10 14:23:06 -0700

    Get the trigger timestamp from the event instead of the TI bank.  Formatting.

--- include/dst/HpsEventBuilder.h
+++ include/dst/HpsEventBuilder.h
@@ -1,15 +1,14 @@
 /**
- *	@file   HpsEventBuilder.h
- *	@author Omar Moreno <omoreno1@ucsc.edu>
- *		    Santa Cruz Institute for Particle Physics
- *		    University of California, Santa Cruz
- *	@date   December 31, 2013
+ *  @file   HpsEventBuilder.h
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *          Santa Cruz Institute for Particle Physics
+ *          University of California, Santa Cruz
+ *  @date   December 31, 2013
  *
  */
 
 #ifndef __HPS_EVENT_BUILDER_H__
 #define __HPS_EVENT_BUILDER_H__
-
 
 //-----------//
 //--- DST ---//
@@ -19,7 +18,6 @@
 #include <EcalDataWriter.h>
 #include <MCParticleDataWriter.h>
 #include <GblDataWriter.h>
-//#include <GblTrackWriter.h>
 #include <HpsParticleDataWriter.h>
 #include <TriggerData.h>
 
@@ -31,42 +29,49 @@
 
 class HpsEventBuilder : public EventBuilder {
 
-	public: 
+    public: 
+
+        /** Constructor */
+        HpsEventBuilder(); 
+
+        /** Destructor */
+        ~HpsEventBuilder(); 
 
         /**
-         *  Default Constructor
-         */
-		HpsEventBuilder(); 
-
-        /**
-         *  Destructor
-         */
-		~HpsEventBuilder(); 
-
-		/**
-         *  Write collections present in the LCEvent to an HpsEvent.
+         *  Write collections encapsulated by an LCEvent object to an 
+         *  {@link HpsEvent} object.
          *
-         *  @param event - The LCEvent to process
-         *  @param hps_event - The HpsEvent to write the LCEvent data to
+         *  @param lc_event LCEvent to process.
+         *  @param hps_event The {@link HpsEvent} to write the LCEvent data to.
          */
-		void makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event); 
+        void makeHpsEvent(EVENT::LCEvent* lc_event, HpsEvent* hps_event); 
 
-		/**
+        /**
+         * Write event specific variables to an {@link HpsEvent}.
+         * 
+         * @param lc_event  LCSim event from which the event information is
+         *                  retrieved.
+         * @param hps_event {@link HpsEvent} that the event information will
+         *                  written to. 
+         */
+        void writeEventData(EVENT::LCEvent* lc_event, HpsEvent* hps_event); 
+
+        /**
          *  Set the strenght of the magnetic field during the run.  This is 
          *  currently used by GBL only and will be removed when the B field
          *  information can be retrieved from the recon file.
          *
          *  @param b_field - The strength of the magnetic field in tesla
          */
-		void setBField(const double);
-		
+        void setBField(const double);
+        
         /**
          *  Enable/disble the processing of SVT tracks with GBL.
          *
          *  @param run_gbl - true to enable GBL, false otherwise
          */
         void runGbl(const bool run_gbl) { this->run_gbl = run_gbl; };
-	
+    
         /**
          *  Enable/disable the creation of an HpsEvent using recon files 
          *  containing ECal data only.
@@ -75,19 +80,19 @@ class HpsEventBuilder : public EventBuilder {
          */    
         void writeEcalOnly(const bool ecal_only) { this->ecal_only = ecal_only; }; 
 
-	private:
+    private:
 
-		SvtDataWriter* svt_writer; 
-		EcalDataWriter* ecal_writer; 
-		MCParticleDataWriter* mc_particle_writer;
-		HpsParticleDataWriter* particle_writer; 
-		GblDataWriter* gbl_data_writer;
+        SvtDataWriter* svt_writer; 
+        EcalDataWriter* ecal_writer; 
+        MCParticleDataWriter* mc_particle_writer;
+        HpsParticleDataWriter* particle_writer; 
+        GblDataWriter* gbl_data_writer;
         TriggerData* hps_trigger_data;
 
         IMPL::LCCollectionVec* trigger_data;
         EVENT::LCGenericObject* trigger_datum;  
 
-		bool run_gbl;
+        bool run_gbl;
         bool ecal_only;
 
 }; // HpsEventBuilder
--- src/dst/HpsEventBuilder.cxx
+++ src/dst/HpsEventBuilder.cxx
@@ -1,10 +1,10 @@
 /**
- *	@file   HpsEventBuilder.cxx
- *	@brief
- *	@author Omar Moreno <omoreno1@ucsc.edu>
- *		    Santa Cruz Institute for Particle Physics
- *			University of California, Santa Cruz
- *	@date   January 6, 2014
+ *  @file   HpsEventBuilder.cxx
+ *  @brief
+ *  @author Omar Moreno <omoreno1@ucsc.edu>
+ *          Santa Cruz Institute for Particle Physics
+ *          University of California, Santa Cruz
+ *  @date   January 6, 2014
  *
  */
 
@@ -20,19 +20,19 @@ HpsEventBuilder::HpsEventBuilder()
       trigger_data(NULL),
       trigger_datum(NULL),
       run_gbl(false),
-      ecal_only(false)
-{}
+      ecal_only(false) {
+}
 
 HpsEventBuilder::~HpsEventBuilder() {
-	delete svt_writer;
-	delete ecal_writer; 	
-	delete mc_particle_writer;
-	delete particle_writer; 
-	delete gbl_data_writer;
+    delete svt_writer;
+    delete ecal_writer;     
+    delete mc_particle_writer;
+    delete particle_writer; 
+    delete gbl_data_writer;
 }
 
 void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event) {
-	
+    
     // Clear the HpsEvent from all previous information
     hps_event->Clear();
 
@@ -42,17 +42,48 @@ void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event) {
     //std::cout << event->getCollectionNames()->size() << std::endl;
     if (event->getCollectionNames()->size() <= 2) return;  
 
-	// Set the event number
-	hps_event->setEventNumber(event->getEventNumber());
-    //std::cout << event->getEventNumber() << std::endl;
+    this->writeEventData(event, hps_event); 
 
-	// Set the run number
-	hps_event->setRunNumber(event->getRunNumber());
+    // Write Ecal data to the HpsEvent
+    ecal_writer->writeData(event, hps_event); 
+
+    // If only Ecal data is going to be written to the DST, skip the rest
+    // of the writers
+    if (ecal_only) return;
+
+    // Write SVT data to the HpsEvent
+    svt_writer->writeData(event, hps_event);
+
+    // Write MC particle data to the HpsEvent
+    mc_particle_writer->writeData(event, hps_event);
+
+    // Write the HpsParticle data to the HpsEvent
+    particle_writer->writeData(event, hps_event); 
+
+    // If GBL has been enabled, process the SVT tracks using GBL and write the
+    // data to the HpsEvent
+    if (!run_gbl) return;
+
+    // Write info used for GBL to the HpsEvent
+    gbl_data_writer->writeData(event, hps_event);
+
+}
+
+void HpsEventBuilder::writeEventData(EVENT::LCEvent* lc_event, HpsEvent* hps_event) { 
+    
+    // Set the event number
+    hps_event->setEventNumber(lc_event->getEventNumber());
+
+    // Set the run number
+    hps_event->setRunNumber(lc_event->getRunNumber());
+
+    // Set the trigger timestamp 
+    hps_event->setTriggerTimeStamp(lc_event->getTimeStamp()); 
 
     // Set the trigger data
     try { 
-        trigger_data = (IMPL::LCCollectionVec*) event->getCollection("TriggerBank"); 
-	} catch(EVENT::DataNotAvailableException e){
+        trigger_data = (IMPL::LCCollectionVec*) lc_event->getCollection("TriggerBank"); 
+    } catch(EVENT::DataNotAvailableException e){
     }
 
     //std::cout << "Number of trigger elements: " << trigger_data->getNumberOfElements() << std::endl;
@@ -63,8 +94,6 @@ void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event) {
         if (trigger_datum->getIntVal(0) == 0xe10a) { 
            
             hps_trigger_data = new TriggerData(trigger_datum); 
-            //std::cout << "Trigger Time: " << hps_trigger_data->getTime() << std::endl; 
-            hps_event->setTriggerTimeStamp(hps_trigger_data->getTime());
             hps_event->setSingle0Trigger((int) hps_trigger_data->isSingle0Trigger());
             hps_event->setSingle1Trigger((int) hps_trigger_data->isSingle1Trigger());
             hps_event->setPair0Trigger((int) hps_trigger_data->isPair0Trigger());
@@ -76,30 +105,6 @@ void HpsEventBuilder::makeHpsEvent(EVENT::LCEvent* event, HpsEvent* hps_event) {
             break;
         }
     }
-
-    // Write Ecal data to the HpsEvent
-	ecal_writer->writeData(event, hps_event); 
-
-    // If only Ecal data is going to be written to the DST, skip the rest
-    // of the writers
-    if (ecal_only) return;
-
-	// Write SVT data to the HpsEvent
-	svt_writer->writeData(event, hps_event);
-
-	// Write MC particle data to the HpsEvent
-	mc_particle_writer->writeData(event, hps_event);
-
-	// Write the HpsParticle data to the HpsEvent
-	particle_writer->writeData(event, hps_event); 
-
-	// If GBL has been enabled, process the SVT tracks using GBL and write the
-    // data to the HpsEvent
-    if (!run_gbl) return;
-
-	// Write info used for GBL to the HpsEvent
-	gbl_data_writer->writeData(event, hps_event);
-
 }
 
 void HpsEventBuilder::setBField(const double b_field) {
